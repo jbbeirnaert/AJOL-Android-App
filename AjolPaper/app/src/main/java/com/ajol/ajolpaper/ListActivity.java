d47ca64c95b3b10e7b5105a59de56a192f0406ea
Owen Gallagher <owengall@icloud.com>
2018-03-29 16:53:02 -0400

    Created android project
    > used map activity
    > layouts are based on RelativeLayout (for some reason I couldn’t get
    them to use ConstraintLayout)
    > started the list layout
    > started the list item layout

---
+++ AjolPaper/app/src/main/java/com/ajol/ajolpaper/ListActivity.java
@@ -0,0 +1,17 @@
+package com.ajol.ajolpaper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by owengallagher on 3/29/18.
+ */
+
+public class ListActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+    }
+}
