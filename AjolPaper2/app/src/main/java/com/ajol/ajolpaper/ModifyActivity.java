c0df5df9daf2413939308c833ab7138f110a00c3
Linh Dang <danglt15@juniata.edu>
2018-04-12 22:53:02 -0400

    Pulling images from the gallerry

--- AjolPaper2/app/src/main/java/com/ajol/ajolpaper/ModifyActivity.java
+++ AjolPaper2/app/src/main/java/com/ajol/ajolpaper/ModifyActivity.java
@@ -1,7 +1,18 @@
 package com.ajol.ajolpaper;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,45 +21,73 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by owengallagher on 3/29/18.
+ * Updated by Linh Dang on 4/12/18.
  */
 
-//Owen: HEY LINH! See this tutorial I found for an example of pulling images from the gallery: http://www.itcuties.com/android/pick-image-from-gallery/
-    /*
 
-    Here's what I learned so far:
-
-    //open camera or gallery with implicit intent
-    startActivityForResult(intent, LOAD_IMAGE_RESULTS);
-
-    //handle returned image
-    onActivityResult(...) {
-        super(...);
-
-        Uri pickedImage = data.getData(); // I think we can store this uri in the database to refer to an image!
-
-        //store pickedImage in database
-        ...
-    }
-
-     */
-
-public class ModifyActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class ModifyActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
     private GoogleMap mMap;
+    private static int LOAD_IMAGE_RESULTS = 1;
+    private Button choose_button;
+    private ImageView image;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify);
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        //...
+        choose_button = (Button)findViewById(R.id.choose_button);
+        choose_button.setOnClickListener(this);
+        image = (ImageView)findViewById(R.id.image);
+
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri imageUri = data.getData();
+        InputStream imageStream = null;
+        try {
+            imageStream = getContentResolver().openInputStream(imageUri);
+            ImageView imageView = (ImageView) findViewById(R.id.photo_preview);
+            imageView.setImageBitmap(BitmapFactory.decodeStream(imageStream));
+        } catch (FileNotFoundException e) {
+            // Handle the error
+        } finally {
+            if (imageStream != null) {
+                try {
+                    imageStream.close();
+                } catch (IOException e) {
+                    // Ignore the exception
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Create the Intent for Image Gallery.
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // Start new activity with the LOAD_IMAGE_RESULTS to handle back the results when image is picked from the Image Gallery.
+        startActivityForResult(i, LOAD_IMAGE_RESULTS);
+    }
+
 
     /**
      * Manipulates the map once available.
@@ -68,4 +107,5 @@ public class ModifyActivity extends AppCompatActivity implements OnMapReadyCallb
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 }
