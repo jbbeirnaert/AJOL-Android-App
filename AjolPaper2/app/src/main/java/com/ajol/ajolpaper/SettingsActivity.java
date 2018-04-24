be92b24b6f1711564a5e390137ad3495627715f9
Owen Gallagher <owengall@icloud.com>
2018-04-24 14:40:29 -0400

    List passes image and isDefault to ModifyAdd

--- AjolPaper2/app/src/main/java/com/ajol/ajolpaper/SettingsActivity.java
+++ AjolPaper2/app/src/main/java/com/ajol/ajolpaper/SettingsActivity.java
@@ -38,6 +38,7 @@ public class SettingsActivity extends FragmentActivity implements OnMapReadyCall
     public static final String WALLPAPER_BUNDLE_Y = "y";
     public static final String WALLPAPER_BUNDLE_R = "r";
     public static final String WALLPAPER_BUNDLE_IMG = "img";
+    public static final String WALLPAPER_BUNDLE_DEFAULT = "default";
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
